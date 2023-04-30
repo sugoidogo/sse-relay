d0f4a7b5588ff3ad39d03ee359845adc3e1d5dcc
josephsmendoza <josephsmendoza@gmai.com>
2023-04-30 11:12:59 -0700

    refactor: remove dispatch and respect network config

--- sser.py
+++ sser.py
@@ -1,6 +1,7 @@
 from os import environ
 configPath=environ.get('SSER_CONFIG_PATH','sser.ini')
 print('configPath =',configPath)
+
 from configparser import ConfigParser
 config=ConfigParser()
 try:
@@ -13,15 +14,11 @@ except:
             'IP':'localhost',
             'Port':4000
         }
-        config['LOG']={
-            'message':False
-        }
         config.write(configFile)
 
 print('config loaded, initializing server')
 
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
-pathFunctions={} # Dict{requestlineregex:function}
 sseClients={} # dict[path]=list[bytesio]
 class ServerSentEventsRelay(BaseHTTPRequestHandler):
     protocol_version='HTTP/1.1'
@@ -30,69 +27,40 @@ class ServerSentEventsRelay(BaseHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin','*')
         super().end_headers()
-    def do_DISPATCH(self):
-        from re import match
-        for requestline, function in pathFunctions.items():
-            if(match(requestline,self.requestline)):
-                return function(self)!=False
         return False
     def do_GET(self):
-        self.do_DISPATCH() or super().do_GET()
-    def do_HEAD(self):
-        self.do_DISPATCH() or super().do_HEAD()
+        if self.path not in sseClients:
+            sseClients[self.path]=[]
+        sseClients[self.path].append(self.wfile)
+        request.send_response(200)
+        request.send_header('Content-Type','text/event-stream')
+        request.end_headers()
     def do_POST(self):
-        self.do_DISPATCH()
-    def do_PUT(self):
-        self.do_DISPATCH()
-    def do_DELETE(self):
-        self.do_DISPATCH()
+        if(self.path not in sseClients):
+            return self.send_error(404)
+        for wfile in sseClients[self.path]:
+            if wfile.closed:
+                sseClients[self.path].remove(wfile)
+                continue
+            if 'id' in self.headers:
+                wfile.write(('id: '+self.headers['id']+'\n').encode())
+            if 'event' in self.headers:
+                wfile.write(('event: '+self.headers['event']+'\n').encode())
+            message=request.rfile.read(int(request.headers['Content-Length'])).decode()
+            for part in message.split('\n'):
+                wfile.write(('data: '+part+'\n').encode())
+            wfile.write('\n'.encode())
+            wfile.flush()
+        if(len(sseClients[self.path])==0):
+            return self.send_error(404)
+        self.send_response_only(200)
     def do_OPTIONS(self):
-        self.do_DISPATCH()
-def sseSend(path:str,message:str,event:str=None,id:str=None):
-    if(path not in sseClients):
-        return False
-    if(len(sseClients[path])==0):
-        return False
-    sent=False
-    for wfile in sseClients[path]:
-        if wfile.closed:
-            continue
-        if id:
-            wfile.write(('id: '+id+'\n').encode())
-        if event:
-            wfile.write(('event: '+event+'\n').encode())
-        for part in message.split('\n'):
-            wfile.write(('data: '+part+'\n').encode())
-        wfile.write('\n'.encode())
-        wfile.flush()
-        sent=True
-    return sent
-def sseGet(request:ServerSentEventsRelay):
-    if request.path not in sseClients:
-        sseClients[request.path]=[]
-    sseClients[request.path].append(request.wfile)
-    request.send_response(200)
-    request.send_header('Content-Type','text/event-stream')
-    request.end_headers()
-def ssePost(request:ServerSentEventsRelay):
-    message=request.rfile.read(int(request.headers['Content-Length'])).decode()
-    event=request.headers['event']
-    id=request.headers['id']
-    if not sseSend(request.path,message,event,id):
-        request.send_error(404)
-    else:
-        request.send_response(204)
-        if(config['LOG']['message']):
-            print({'path':request.path,'event':event,'message':message,'id':id})
-    request.end_headers()
-def sseOptions(request:ServerSentEventsRelay):
-    request.send_response(204)
-    request.send_header('Access-Control-Allow-Headers', 'event,id')
-    request.end_headers()
-pathFunctions['GET /']=sseGet
-pathFunctions['POST /']=ssePost
-pathFunctions['OPTIONS /']=sseOptions
-server=ThreadingHTTPServer(('0.0.0.0',4000),ServerSentEventsRelay)
+        self.send_response(204)
+        self.send_header('Access-Control-Allow-Headers', 'event,id')
+        self.end_headers()
+
+bind=(config['NETWORK']['IP'],config['NETWORK'].getint('Port'))
+server=ThreadingHTTPServer(bind,ServerSentEventsRelay)
 from threading import Thread
 server=Thread(target=server.serve_forever)
 server.start()
